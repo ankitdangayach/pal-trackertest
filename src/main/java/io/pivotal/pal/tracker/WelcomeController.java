93387a90bafce961e230b7acbe7bd8ab6f21b452
ankit dangayach <ankit.dangayach@loblaw.ca>
2019-09-11 16:27:50 -0400

    Updated Pal Tracker Application Message

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
     @GetMapping("/")
     public String sayHello(){
-        return "hello";
+        return "hello Ankit";
     }
 }
